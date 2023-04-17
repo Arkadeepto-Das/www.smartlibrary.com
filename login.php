92f9c8869c5fa6a1b141fe5b17a09ca40619049b
Arkadeepto-Das <arkadeepto.das@innoraft.com>
2023-04-17 10:33:59 +0530

    MVC_framework:Made the validation for login.

--- login.php
+++ login.php
@@ -1,8 +1,41 @@
 <?php
-  require 'vendor/autoload.php';
-  require './classes/EnvHandler.php';
-  $userName = $_POST["username"];
+  if (!isset($_SESSION)) {
+    session_start();
+  }
+  require_once 'vendor/autoload.php';
+  $userName = $_POST["userName"];
   $password = $_POST["password"];
   $env = new EnvHandler();
-  $env->fetchEnvValues();
-?>
+  $envArray = $env->fetchEnvValues();
+  $query = new Database($envArray["serverName"], $envArray["userName"], $envArray["password"], $envArray["dbName"]);
+  $validate = new Validation();
+  $userDetails = $query->userDetails($userName);
+  $response = [];
+  if ($userDetails === NULL || $userName != $userDetails["Username"]) {
+    $response['status'] = 0;
+    $response['userName'] = "Username doesn't exist.";
+    echo json_encode($response);
+    exit();
+  }
+  else {
+    if ($validate->passwordValidation($password) === FALSE) {
+      $response['status'] = 0;
+      $response['password'] = "Password should be of min 8 characters with atleast 1 uppercase, 1 lowercase, 1 digit and 1 special character.";
+      echo json_encode($response);
+      exit();
+    }
+    else {
+      if ($password != $userDetails["Password"]) {
+        $response['status'] = 0;
+        $response['password'] = "Incorrect password";
+        echo json_encode($response);
+        exit();
+      }
+      else {
+        $response['status'] = 1;
+        $_SESSION["userName"] = $userDetails["Username"];
+        $response['role'] = $userDetails["Role"];
+        echo json_encode($response);
+      }
+    }
+  }
